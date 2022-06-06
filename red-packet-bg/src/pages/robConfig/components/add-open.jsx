731f5f3338eb97ccf4d346b4cc68a9e09d0c0bd2
王明磊 <wangminglei@wangmingleideMacBook-Pro.local>
2022-06-06 17:24:47 +0800

    拆红包配置更新

---
+++ red-packet-bg/src/pages/robConfig/components/add-open.jsx
@@ -0,0 +1,123 @@
+//对话框表单组件
+import React, { Component } from 'react'
+import { Modal, Input, Form, message } from 'antd'
+import { reqAddOpen, reqEditOpen } from '../../../api'
+const { Item } = Form
+
+export default class ModalComponent extends Component {
+	formRef = React.createRef()
+	constructor(props) {
+		super(props)
+		this.state = {
+			flag: false,
+			type: '',
+			id: '',
+		}
+	}
+	componentWillMount() {
+		let { flag, id, name, begin, end, probability, type, dataSource, grabId } = this.props
+		this.setState({
+			flag,
+			id,
+			name,
+			begin,
+			end,
+			probability,
+			type,
+			dataSource,
+			grabId
+		})
+	}
+	componentDidMount() {
+		let { name, begin, end, probability } = this.state
+		this.formRef.current.setFieldsValue({ name })
+		this.formRef.current.setFieldsValue({ begin })
+		this.formRef.current.setFieldsValue({ end })
+		this.formRef.current.setFieldsValue({ probability })
+	}
+	closeClear = () => {
+		let { closeModal } = this.props
+		this.formRef.current.resetFields() //清空表单
+		closeModal()
+	}
+
+	render() {
+		let { flag, type } = this.state
+		return (
+			<Modal 
+				title={type + '奖项'}
+				visible={flag}
+				onOk={this.handleOk}
+				onCancel={() => this.closeClear()}
+				cancelText="取消"
+				okText="确定"
+			>
+				{
+					<Form labelCol={{span: 5}} ref={this.formRef}>
+						<Item name="name" label="奖项" rules={[{ required: true, message: '奖项不可以为空!' }]}>
+							<Input placeholder="请输入奖项！" onChange={this.changeName} />
+						</Item>
+						<Item label="中奖金额区间" style={{marginBottom: '0px'}}>
+							<Item name="begin" label="起始值" rules={[{ required: true, message: '起始值不可以为空!' }]}>
+								<Input placeholder="请输入起始值！" onChange={this.changeBegin} />
+							</Item>
+							<Item name="end" label="结束值" rules={[{ required: true, message: '起始值不可以为空!' }]}>
+								<Input placeholder="请输入终止值！" onChange={this.changeEnd} />
+							</Item>
+						</Item>
+						<Item name="probability" label="中奖概率" rules={[{ required: true, message: '中奖概率不可以为空!' }]}>
+							<Input placeholder="请输入中奖概率！" style={{width:'95%'}} onChange={this.changeProbability} />
+						</Item>
+						<div style={{float:'right',marginTop:'-50px'}}>%</div>
+					</Form>
+				}
+			</Modal>
+		)
+	}
+	changeName = e => {
+		this.setState({
+			name: e.target.value,
+		})
+	}
+	changeBegin = e => {
+		this.setState({
+			begin: e.target.value,
+		})
+	}
+	changeEnd = e => {
+		this.setState({
+			end: e.target.value,
+		})
+	}
+	changeProbability = e => {
+		this.setState({
+			probability: e.target.value,
+		})
+	}
+
+	handleOk = async () => {
+		let { closeModal } = this.props
+		let { id, name, begin, end, probability, type, grabId } = this.state
+		if (!name) return message.info('抢红包下注金额不可以为空！')
+		if (!begin) return message.info('起始值不可以为空！')
+		if (!end) return message.info('终止值不可以为空！')
+		if (!probability) return message.info('中奖概率不可以为空！')
+		let params = {
+			id,
+			name,
+			begin: Number(begin),
+			end: Number(end),
+			probability: Math.round(probability*100)/100,
+			grabId
+		}
+		let result = type === '新增' ? await reqAddOpen(params) : await reqEditOpen(params)
+		if (result.code === 0 && result.data.code === 0) {
+			message.success(type === '新增' ? '奖项添加成功！' : '奖项修改成功！')
+			this.formRef.current.setFieldsValue({ name: undefined,begin: undefined,end: undefined,probability:undefined}) //给表单设置值
+			this.formRef.current.resetFields() //清空表单
+		} else {
+			return message.error(result.data.msg)
+		}
+		closeModal()
+	}
+}
--- red-packet-bg/src/pages/robConfig/components/openConfig.jsx
+++ red-packet-bg/src/pages/robConfig/components/openConfig.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import { Modal, Card, Table, Button, message } from 'antd'
 import { reqOpenList, reqDeleteOpen  } from '../../../api'
 import { PlusCircleOutlined } from '@ant-design/icons'
+import AddOpen from './add-open'
 
 export default class ModalComponent extends Component {
 	formRef = React.createRef()
@@ -14,6 +15,8 @@ export default class ModalComponent extends Component {
 			dataSource: [],
 			isDeleteVisible: false,
 			isModalVisible: false,
+			pageNumber: 1,
+            pageSize: 10,
 		}
 	}
 	componentWillMount() {
@@ -21,13 +24,17 @@ export default class ModalComponent extends Component {
 		this.setState({
 			flag,
 			robId,
+		},()=>{
+			this.getDataList()
 		})
-		this.getDataList()
 	}
+	// 获取当前抢红包id下的拆红包列表
 	getDataList = async () => {
+		let { pageNumber,pageSize,robId } = this.state
 		let params = {
-			current: 0,
-			size: 100
+			current: pageNumber,
+			size: pageSize,
+			grabId: robId
 		}
 		let result = await reqOpenList(params)
 		if (result.code === 0) {
@@ -49,88 +56,154 @@ export default class ModalComponent extends Component {
 	}
 
 	render() {
-		let { flag, dataSource, id, isDeleteVisible } = this.state
+		let { flag, dataSource, id, isDeleteVisible, pageNumber, pageSize, dataTotal } = this.state
 		// card的左侧
 		const title = (
 			<span>
-				<Button type={'primary'} icon={<PlusCircleOutlined/>} onClick={() => this.oppModal('新增')}>添加</Button>
+				<Button type={'primary'} icon={<PlusCircleOutlined/>} onClick={() => this.newModal('新增')}>添加</Button>
 			</span>
 		)
 		return (
-		<Modal 
-			title="拆红包配置"
-			visible={flag}
-			onOk={this.handleOk}
-			onCancel={() => this.closeClear()}
-			cancelText="取消"
-			okText="确定"
-		>
-			{
-				<Card title={title}>
-					<Table
-						bordered
-						rowKey="id"
-						dataSource={dataSource}
-						columns={[
-							{
-								title: '奖项',
-								align: 'center',
-								dataIndex: 'name',
-								key: 'name',
-							},
-							{
-								title: '拆红包中奖区间',
-								align: 'center',
-								key: 'id',
-								render: reload =>  {
-									return (
-										<span>{'[' + reload.begin + ',' + reload.end + ']'}</span>
-									)
-								}
-							},
-							{
-								title: '中奖概率',
-								align: 'center',
-								key: 'id',
-								render: reload =>  {
-									return (
-										<span>{reload.probability + '%'}</span>
-									)
-								}
-							},
-							{
-								title: <span style={{ fontWeight: 700 }}>操作</span>,
-								key: 'id',
-								align: 'center',
-								width: '20%',
-								render: reload => {
-									return (
-										<span>
-											<Button type={'link'} onClick={() => this.oppModal('修改',reload)}>编辑</Button>
-											<Button type={'link'} onClick={() => this.deleteModal(reload.id)}>删除</Button>
-										</span>
-									)
+			<Modal 
+				title="拆红包配置"
+				visible={flag}
+				onOk={this.handleOk}
+				onCancel={() => this.closeClear()}
+				cancelText="取消"
+				okText="确定"
+				width="80%"
+			>
+				{
+					<Card title={title}>
+						<Table
+							bordered
+							rowKey="id"
+							dataSource={dataSource}
+							pagination={{current: pageNumber,pageSize: pageSize, 
+								showQuickJumper: false, 
+								showSizeChanger: true, 
+								pageSizeOptions: ["5","10","15","20"],
+								total: this.state.dataTotal,
+								onChange: this.onPageChange,
+								onShowSizeChange: this.onPageChange,
+								showTotal: (e) => {return `共 ${dataTotal} 条`}}}
+							scroll={{ y: '40vh' }}
+							columns={[
+								{
+									title: '奖项',
+									align: 'center',
+									dataIndex: 'name',
+									key: 'name',
 								},
-							},
-						]}
-					/>
-					<Modal
-						title="删除"
-						visible={isDeleteVisible}
-						onOk={() => this.deleteRobConfig(id)}
-						onCancel={() => {this.setState({
-							isDeleteVisible: false
-						})}}
-					>
-						<span>确认删除该项配置吗?</span>
-					</Modal>
-					{this.showModal(this.state.isModalVisible, dataSource)}
-				</Card>
-			}
-		</Modal>
+								{
+									title: '拆红包中奖区间',
+									align: 'center',
+									key: 'id',
+									render: reload =>  {
+										return (
+											<span>{'[' + reload.begin + ',' + reload.end + ']'}</span>
+										)
+									}
+								},
+								{
+									title: '中奖概率',
+									align: 'center',
+									key: 'id',
+									render: reload =>  {
+										return (
+											<span>{reload.probability + '%'}</span>
+										)
+									}
+								},
+								{
+									title: <span style={{ fontWeight: 700 }}>操作</span>,
+									key: 'id',
+									align: 'center',
+									width: '200px',
+									render: reload => {
+										return (
+											<span>
+												<Button type={'link'} onClick={() => this.newModal('修改',reload)}>编辑</Button>
+												<Button type={'link'} onClick={() => this.deleteModal(reload.id)}>删除</Button>
+											</span>
+										)
+									},
+								},
+							]}
+						/>
+					</Card>
+				}
+				<Modal
+					title="删除"
+					visible={isDeleteVisible}
+					onOk={() => this.deleteRobConfig(id)}
+					onCancel={() => {this.setState({
+						isDeleteVisible: false
+					})}}
+				>
+					<span>确认删除该项配置吗?</span>
+				</Modal>
+				{this.showModal(this.state.isModalVisible, dataSource)}
+			</Modal>
 		)
 	}
-
+	onPageChange = (pageNumber, pageSize) => {
+        this.setState({
+            pageNumber,
+            pageSize,
+        },() => {
+			this.getDataList()
+		})
+    }
+	newModal = (type, data) => {
+		if (data) {
+			// 编辑
+			let { id, name, begin, end, probability } = data
+			this.setState({
+				id: id,
+				isModalVisible: true,
+				modalType: type,
+				name,
+				begin,
+				end,
+				probability
+			})
+		} else {
+			// 新增
+			this.setState({
+				isModalVisible: true,
+				modalType: type,
+			})
+		}
+	}
+	// 新增or编辑 拆红包配置项
+	showModal = (flag, data) => {
+		let { id, name, begin, end, probability, modalType, robId } = this.state
+		if (flag) {
+			return (
+				<AddOpen
+					flag={flag}
+					dataSource={data}
+					closeModal={() => this.setState({isModalVisible: false,})}
+					grabId={robId}
+					id={id}
+					name={name}
+					begin={begin}
+					end={end}
+					probability={probability}
+					type={modalType}
+				/>
+			)
+		}
+	}
+	// 删除 拆红包配置项
+	deleteModal = (id) => {
+		this.setState({
+			isDeleteVisible: true,
+			id,
+		})
+	}
+	// 删除 拆红包配置项
 	deleteRobConfig = async (id) => {
 		const result = await reqDeleteOpen(id)
 		if (result.code === 0) {
